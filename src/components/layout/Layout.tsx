8fd932426a1666dabf529060eac54bc7c7a2d21a
Faizal-Basha-S <faizalbashafaizalbasha07@gmail.com>
2025-05-10 17:26:55 +0530

    Update Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -14,7 +14,7 @@ const Layout = ({ children }) => {
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-1 pt-16">
-        <div className="w-full max-w-[1440px] mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="w-full max-w-[1200px] mx-auto px-4 sm:px-6 lg:px-8">
           {children}
         </div>
       </main>
