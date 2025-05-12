6e3c90d981a8158d9a7fd4114e7e479549e57ee8
Faizal-Basha-S <faizalbashafaizalbasha07@gmail.com>
2025-05-12 16:37:33 +0530

    Update FAQs.tsx

--- src/pages/FAQs.tsx
+++ src/pages/FAQs.tsx
@@ -8,7 +8,7 @@ import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/
 const FAQs = () => {
   return (
     <Layout>
-      <div className="container mx-auto px-4 py-8 max-w-4xl">
+      <div className="container mx-auto  px-4 pt-24 max-w-4xl">
         {/* Breadcrumb */}
         <div className="flex items-center gap-2 mb-8 text-sm">
           <Link to="/" className="text-muted-foreground hover:text-primary flex items-center">
