ec4a077227b5da1bb25312a35b390961e9afaaa2
Faizal-Basha-S <faizalbashafaizalbasha07@gmail.com>
2025-05-29 11:15:26 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,6 +3,7 @@ import Hero from "../components/home/Hero";
 import FeaturedCars from "../components/cars/FeaturedCars";
 import FeaturedBikes from "../components/bikes/FeaturedBikes";
 import Layout from "../components/layout/Layout";
+import FloatingVideoButton from "@/components/appointment/FloatingVideoButton";
 import { 
   ArrowRight, 
   Car, 
@@ -403,7 +404,8 @@ const Index = () => {
           </div>
         </div>
       </section>
-      
+      {/* Floating Video Button */}
+      <FloatingWhatsappButton onClick={() => setIsVideoModalOpen(true)} />
       <AIFloatingButton />
     </Layout>
   );
