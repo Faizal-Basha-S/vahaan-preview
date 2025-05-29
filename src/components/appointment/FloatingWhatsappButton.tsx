08c3b8c9a9588d8a11eb1d38a83b37545558c4fb
Faizal-Basha-S <faizalbashafaizalbasha07@gmail.com>
2025-05-29 13:07:37 +0530

    Update FloatingWhatsappButton.tsx

--- src/components/appointment/FloatingWhatsappButton.tsx
+++ src/components/appointment/FloatingWhatsappButton.tsx
@@ -35,8 +35,8 @@ const FloatingWhatsappButton = () => {
             >
               <svg 
                 xmlns="http://www.w3.org/2000/svg" 
-                width="24" 
-                height="24" 
+                width="25" 
+                height="25" 
                 fill="currentColor" 
                 viewBox="0 0 16 16"
               >
